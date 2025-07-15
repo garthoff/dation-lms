cdcc9d663601f5805a4d988407356473c5386333
Tim Garthoff <gitlab@garthoff.com>
2025-07-15 18:28:13 +0200

    ..

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
+import os
+
+import re, ast
+
+# get version from __version__ variable in app's/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+with open('branded_lms/__init__.py', 'rb') as f:
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
 
 setup(
     name='branded_lms',
-    version='1.0.0',
-    description='Frappe LMS with custom branding support',
-    author='Tim Garthoff',
+    version=version,
+    description='App for LMS',
+    author='Tim',
+    author_email='revant.one@gmail.com',
     packages=find_packages(),
-    include_package_data=True,
     zip_safe=False,
-    install_requires=['frappe'],
+    include_package_data=True,
+    install_requires=("frappe",),
 )
